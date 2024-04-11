ba974c7579c5447fadd71ffed64f64daba2a8a66
hahns_pena <hpena02@calpoly.edu>
2024-04-11 16:50:45 -0700

    Posting

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -39,23 +39,49 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// Users
+// By Name
 const findUserByName = (name) => {
     return users["users_list"].filter(
       (user) => user["name"] === name
     );
   };
   
-  app.get("/users", (req, res) => {
-    const name = req.query.name;
-    if (name != undefined) {
-      let result = findUserByName(name);
-      result = { users_list: result };
-      res.send(result);
-    } else {
-      res.send(users);
-    }
-  });
+  //By ID
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+app.get("/users", (req, res) => {
+const name = req.query.name;
+if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+} else {
+    res.send(users);
+}
+});
+
+// Add User
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
+
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+        res.send();
+});
 
 app.listen(port, () => {
   console.log(
